173a6997f9521abcf42d4ddc48150d5fe9c22ab6
Eliezer Romero <eliezerjromeroc@gmail.com>
2024-09-29 08:53:36 -0400

    add fn for send email

---
+++ qaroni/mail.py
@@ -0,0 +1,11 @@
+from qaroni.extensions import mail
+from flask_mail import Message
+
+
+def send_mail(recipient: str, asunto:str, body: str):
+    try:
+        mail.send_message(asunto, sender="eliezerfot123@gmail.com", recipients=[recipient], body=body)
+        return "Send email"
+    except Exception as e:
+        print(e)
+        return "Error sending email"
